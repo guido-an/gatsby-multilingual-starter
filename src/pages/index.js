0438de3b01716cb85b8b6530e0d6b22d7f0c9081
Guido <carucciguido@gmail.com>
2019-10-30 16:21:11 +0100

    gatsby multilingual starter

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,30 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+import enMessages from '../data/index/en.json'
+import itMessages from '../data/index/it.json'
+
+const messages = {
+  en: enMessages,
+  it: itMessages
+}
+
+const IndexPage = ({ pathContext: { locale } }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+
+    <h1>{messages[locale].hello}</h1>
+ 
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
 export default IndexPage
+
+
+
+
